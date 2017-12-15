055db331a55588c3122e376cab8e68287861be8c
jhonatacaiob <34169712+jhonatacaiob@users.noreply.github.com>
2017-12-15 14:15:28 -0300

    Update questao 3.py

--- questao 3.py
+++ questao 3.py
@@ -3,5 +3,4 @@ n = int(input("Digite um Numero: "))
 while len(damasceno)<n:
     numero = (damasceno[-1]*damasceno[-2])+1
     damasceno+=[numero]
-jo = " ".join(damasceno)
 print("A SEQUENCIA É", damasceno)
